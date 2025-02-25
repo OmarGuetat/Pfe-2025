70418b8ad0cde8b911d99e41580b7ad58b73773f
OmarGuetat <Genie.om4r@gmail.com>
2025-02-25 14:03:26 +0100

    2-25-2025

--- src/app/hr/hr-home/hr-home.component.ts
+++ src/app/hr/hr-home/hr-home.component.ts
@@ -7,11 +7,12 @@ import { LeaveFormComponent } from '../../employee/leave-form/leave-form.compone
 import { ProfileComponent } from '../../employee/profile/profile.component';
 import { LeaveDashboardComponent } from '../../admin/leave-dashboard/leave-dashboard.component';
 import { UsersDashboardComponent } from '../../admin/users-dashboard/users-dashboard.component';
+import { RequestsUserDashboardComponent } from '../../components/requests-user-dashboard/requests-user-dashboard.component';
 
 
 @Component({
   selector: 'app-hr-home',
-   imports: [LeaveFormComponent,SidebarComponent,CommonModule, FormsModule, ReactiveFormsModule,FooterComponent,ProfileComponent,LeaveDashboardComponent,UsersDashboardComponent],
+   imports: [LeaveFormComponent,SidebarComponent,CommonModule, FormsModule, ReactiveFormsModule,FooterComponent,ProfileComponent,LeaveDashboardComponent,UsersDashboardComponent,RequestsUserDashboardComponent],
   templateUrl: './hr-home.component.html',
   styleUrl: './hr-home.component.css'
 })
@@ -21,6 +22,7 @@ export class HrHomeComponent {
     { label: 'Users Dashboard', component: 'users-dashboard', icon: 'bi-people' },
     { label: 'Leave Dashboard', component: 'leave-dashboard', icon: 'bi-calendar-check' },
     { label: 'Leave Form', component: 'leave-form', icon: 'bi-calendar' },
+    { label: 'Leave Requests', component: 'user-request-dashboard', icon: 'bi-calendar-check' },
     
   ];
   activeComponent = 'leave-form'; 
